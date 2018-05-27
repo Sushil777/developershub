f6f48d18dca9d683523c330fbe1bfb663b6fa710
Sushil777 <sushilmangal731@gmail.com>
2018-05-27 09:23:55 +0530

    Backend setup completed for now

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://sushil:sushil@ds229380.mlab.com:29380/developersconnector"
-};
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
+//importing routes
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 const users = require("./routes/api/users");
 
+//initializing express
 const app = express();
+
+//body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//passport middleware
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
 //DB Config
 const db = require("./config/keys").mongoURI;
@@ -16,6 +28,7 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+//home route
 app.get("/", (req, res) => res.send("Hello World"));
 
 //Use Routes
@@ -23,6 +36,6 @@ app.use("/api/posts", posts);
 app.use("/api/profile", profile);
 app.use("/api/users", users);
 
+//port configurations
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server running at port ${port}`));
